ddc67155bfaab984e59e64a09f539515c52b7944
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2022-01-17 23:50:18 -0800

    linked list

---
+++ javascript/linked-list/linkedList.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let current = this.head;
+    let str = "";
+    while (current) {
+      str += `${current.value} -> `;
+      current = current.next;
+    }
+    str += "NULL";
+    return str;
+  }
+}
+
+const list = new LinkedList();
